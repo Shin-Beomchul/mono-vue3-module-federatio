85e9019f1b2b08ac20d1d0b7577bc436505e04a9
BeomChul <sbc0830@osstem.com>
2022-04-25 15:27:32 +0900

    CSS Priority
    index.ts

--- applications/dml/src/index.ts
+++ applications/dml/src/index.ts
@@ -1,4 +1,10 @@
 (async () => {
-  await import("common/global.scss").catch((e) => console.error("TODO report to Server", e)); // from remote
+  /* remote CSS first(commButton RED)*/
+  await import("@/assets/styles/global.scss"); // green
+  await import("common/global.scss").catch((e) => console.error("TODO report to Server", e)); // red : from remote
+
+  /* remote CSS first(commButton GREEN)*/
+  //   await import("common/global.scss").catch((e) => console.error("TODO report to Server", e)); // red : from remote
+  //   await import("@/assets/styles/global.scss"); // green
   import("./main");
 })();
--- applications/dml/src/types/types.d.ts
+++ applications/dml/src/types/types.d.ts
@@ -1,1 +1,2 @@
 declare module "common/global.scss";
+declare module "@/assets/styles/global.scss";
